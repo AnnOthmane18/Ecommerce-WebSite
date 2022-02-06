10afec9411cc39b456df540d19522b461a954ed4
othmane <othmaneannassi@gmail.com>
2022-02-06 18:11:38 +0100

    responsive product page not 100% completed

--- resources/views/pages/productPage.blade.php
+++ resources/views/pages/productPage.blade.php
@@ -56,14 +56,79 @@
             </div>
         </div>
     </div>
+    <div class="guaranties">
+        <div class=" guaranty world-wide-shipping">
+            <img src="{{asset('imgs/Quranties/freeShipping.webp')}}" alt="">
+            <p>
+                <strong>Free premium worldwide shipping</strong>
+            </p>
+        </div>
+        <div class="secure-payment">
+            <img src="{{asset('imgs/Quranties/securePayment.webp')}}" alt="">
+            <p>
+                <strong>100% secured payment</strong>
+            </p>
+        </div>
+        <div class="shipping24">
+            <img src="{{asset('imgs/Quranties/shipping24.webp')}}" alt="">
+            <p>
+                Shipping all orders in 24-48 hours <strong>24-48 hours</strong>
+            </p>
+        </div>
+        <div class="return30">
+            <img src="{{asset('imgs/Quranties/return.png')}}" alt="">
+            <p>
+                <strong>30 day return policy</strong>
+            </p>
+        </div>
+        <div class="support">
+            <img src="{{asset('imgs/Quranties/support.png')}}" alt="">
+            <p>
+                Expert <strong>support team</strong>
+            </p>
+        </div>
+    </div>
+
+    <div class="benefits">
+        <h1>BENEFITS OF USING BARNER BLUE LIGHT GLASSES</h1>
+        <div class="benefits-blocs">
+            <div class="benefit">
+                <img src="{{asset('imgs/benefits_icons/icon1.webp')}}" alt="">
+                <p>Look cool <br> wherever you go</p>
+            </div>
+            <div class="benefit">
+                <img src="{{asset('imgs/benefits_icons/icons2.webp')}}" alt="">
+                <p> Reduce Eye <br> strain</p>
+            </div>
+            <div class="benefit">
+                <img src="{{asset('imgs/benefits_icons/icons3.webp')}}" alt="">
+                <p>Improve your <br> sleep</p>
+            </div>
+            <div class="benefit">
+                <img src="{{asset('imgs/benefits_icons/icons4.webp')}}" alt="">
+                <p>Enhance your <br> wellbeing</p>
+            </div>
+            <div class="benefit">
+                <img src="{{asset('imgs/benefits_icons/icons5.webp')}}" alt="">
+                <p>Super <br> comfortable</p>
+            </div>
+            <div class="benefit">
+                <img src="{{asset('imgs/benefits_icons/icons6.webp')}}" alt="">
+                <p>Practically <br> wightless</p>
+            </div>
+            <div class="benefit">
+                <img src="{{asset('imgs/benefits_icons/icons7.webp')}}" alt="">
+                <p>Cool  <br> packaging</p>
+            </div>
+            <div class="benefit">
+                <img src="{{asset('imgs/benefits_icons/icons8.webp')}}" alt="">
+                <p>Free <br> Accessories</p>
+            </div>
+        </div>
+    </div>
 </section>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+
 <br>
 
-<!-- @include('layouts.includes.footer') -->
+ @include('layouts.includes.footer') 
